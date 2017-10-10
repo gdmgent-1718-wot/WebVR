f9cac5f973918e3ee11f75d8dd1fa419f93d9ba1
daanmeer <daanmeer@arteveldehs.be>
2017-10-10 08:42:13 +0200

    update

---
+++ enter_vr.js
@@ -0,0 +1,13 @@
+(function () {
+  // switch to stereoscopic mode directly on page load, this needs to be after the a-scene loads.
+  var scene = document.querySelector('a-scene');
+  if (scene.hasLoaded) {
+      scene.enterVR();
+  } else {
+      scene.addEventListener('loaded', function () {
+          setTimeout(function () {
+              scene.enterVR();
+          }, 500);
+      });
+  };
+})();
